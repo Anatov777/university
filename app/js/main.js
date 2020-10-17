6bc1fe7e8524d7c68cd233e66f3a23438bd24ff7
Anatov777 <anatovtimur@gmail.com>
2020-10-17 21:38:35 +0300

    add main page

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    slidesToShow: 3,
+    variableWidth: true,
+  });
+  document.querySelector('.close').addEventListener('click', function(){
+    document.querySelector('.alert').classList.add('closed');
+  })
+});
